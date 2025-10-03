4512d31ac0f3545aad1097b2848a6a52578b8f04
ploymahloy <patrick.mahloy@gmail.com>
2025-10-02 20:15:18 -0400

    mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-function App() {
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import SearchBar from "./components/SearchBar";
+import './App.css';
 
+const queryClient = new QueryClient();
+
+function App() {
   return (
-    <>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <div className="app-container">
+        <h1 className="app-title">
+          Search Suggestions Demo
+        </h1>
+        <SearchBar />
+      </div>
+    </QueryClientProvider>
   )
 }
 
-export default App
+export default App;
